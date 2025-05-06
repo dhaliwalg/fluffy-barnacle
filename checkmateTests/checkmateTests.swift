baff8f0ba1bc9721c1145e5146350bf7deb80aeb
Gurjit Dhaliwal <dhaliwalgurjit022@gmail.com>
2025-05-06 18:30:31 -0400

    Initial Commit

---
+++ checkmateTests/checkmateTests.swift
@@ -0,0 +1,17 @@
+//
+//  checkmateTests.swift
+//  checkmateTests
+//
+//  Created by null on 5/6/25.
+//
+
+import Testing
+@testable import checkmate
+
+struct checkmateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
